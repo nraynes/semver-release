e59e75260c87756ef784444ef15d538b6c8d45f1
nicholasraynes <nicholasraynes@yahoo.com>
2026-04-27 10:06:51 -0400

    chore: debugging statement added for git auth

--- src/tools/git/auth/github.rs
+++ src/tools/git/auth/github.rs
@@ -6,20 +6,22 @@ use crate::Alert;
 
 /// Caches credentials in git for authenticating with Github as remote origin.
 pub fn set_remote(env: &IndexMap<String, String>) -> Result<(), Alert> {
+    let actor = env
+        .get("GITHUB_ACTOR")
+        .ok_or("GITHUB_ACTOR not in environment variables.")?;
+    let token = env
+        .get("GITHUB_TOKEN")
+        .ok_or("GITHUB_TOKEN not in environment variables.")?;
+    let repo = env
+        .get("GITHUB_REPOSITORY")
+        .ok_or("GITHUB_REPOSITORY not in environment variables.")?;
+    println!("actor: {}, token: {}, repo: {}", actor, token, repo);
     Command::new("git")
         .args([
             "remote",
             "set-url",
             "origin",
-            &format!(
-                "https://${}:${}@github.com/{}.git",
-                env.get("GITHUB_ACTOR")
-                    .ok_or("GITHUB_ACTOR not in environment variables.")?,
-                env.get("GITHUB_TOKEN")
-                    .ok_or("GITHUB_TOKEN not in environment variables.")?,
-                env.get("GITHUB_REPOSITORY")
-                    .ok_or("GITHUB_REPOSITORY not in environment variables.")?
-            ),
+            &format!("https://${}:${}@github.com/{}.git", actor, token, repo),
         ])
         .output()?;
     Ok(())
