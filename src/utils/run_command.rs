fd1af6e20dfe08876cbe54411bba6de9bf8622ad
nicholasraynes <nicholasraynes@yahoo.com>
2026-04-27 11:32:38 -0400

    chore: added debug line

--- src/utils/run_command.rs
+++ src/utils/run_command.rs
@@ -11,8 +11,9 @@ where
     let stdout = String::from_utf8(output.stdout)?;
     let stderr = String::from_utf8(output.stderr)?;
     let status = output.status.code().ok_or(&stderr)?;
+    println!("StdOut from {}:\n{}", command, stdout);
+    println!("StdErr from {}:\n{}", command, stdout);
     if status == 0 {
-        println!("StdOut from {}:\n{}", command, stdout);
         return Ok(stdout);
     }
     Err(Alert::from(stderr))
