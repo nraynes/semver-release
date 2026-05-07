02f4874c8300fd6b6ae3256a4fb59d6f0f91b2fd
nicholasraynes <nicholasraynes@yahoo.com>
2026-05-06 20:27:13 -0400

    fix: fixed analyze commits bug and added tests

--- src/tools/analyzer/analyze_commits.rs
+++ src/tools/analyzer/analyze_commits.rs
@@ -16,22 +16,31 @@ pub fn analyze_commits(
     let mut minor = current_version.1;
     let mut patch = current_version.2;
     let mut changes: CommitMap = CommitMap::new();
+    let mut minor_lock = false;
+    let mut patch_lock = false;
     for commit in commits.iter() {
         if let Some(change) = major_changes.check(commit) {
             major += 1;
             minor = 0;
             patch = 0;
             changes.insert(change, commit.clone())?;
+            minor_lock = true;
+            patch_lock = true;
             continue;
         }
         if let Some(change) = minor_changes.check(commit) {
-            minor += 1;
-            patch = 0;
+            if !minor_lock {
+                minor += 1;
+                patch = 0;
+            }
             changes.insert(change, commit.clone())?;
+            patch_lock = true;
             continue;
         }
         if let Some(change) = patch_changes.check(commit) {
-            patch += 1;
+            if !patch_lock {
+                patch += 1;
+            }
             changes.insert(change, commit.clone())?;
             continue;
         }
@@ -46,25 +55,122 @@ pub fn analyze_commits(
 #[cfg(test)]
 mod test {
     use super::*;
-    use semver_common::mock;
+    use std::collections::HashMap;
 
-    #[test]
-    fn test_analyze_commits_valid() {
+    use semver_common::{CommitBucket, mock};
+
+    #[derive(PartialEq, Eq, Hash, Debug)]
+    enum CommitType {
+        MAJOR,
+        MINOR,
+        PATCH,
+        OTHER,
+    }
+
+    fn mock_comparison_map() -> HashMap<CommitType, Vec<Commit>> {
+        let mut comparison_map: HashMap<CommitType, Vec<Commit>> = HashMap::new();
+        comparison_map.insert(CommitType::MAJOR, Vec::new());
+        comparison_map.insert(CommitType::MINOR, Vec::new());
+        comparison_map.insert(CommitType::PATCH, Vec::new());
+        comparison_map.insert(CommitType::OTHER, Vec::new());
+        comparison_map
+    }
+
+    fn mock_commits(
+        commit_list: Vec<(&str, CommitType)>,
+    ) -> (Vec<Commit>, HashMap<CommitType, Vec<Commit>>) {
+        let commits: Vec<Commit> = commit_list
+            .iter()
+            .map(|v| mock::commit::create(v.0))
+            .collect();
+        let mut expected_commits = mock_comparison_map();
+        commit_list.iter().for_each(|v| {
+            match v.1 {
+                CommitType::MAJOR => expected_commits
+                    .get_mut(&CommitType::MAJOR)
+                    .unwrap()
+                    .push(mock::commit::create(v.0)),
+                CommitType::MINOR => expected_commits
+                    .get_mut(&CommitType::MINOR)
+                    .unwrap()
+                    .push(mock::commit::create(v.0)),
+                CommitType::PATCH => expected_commits
+                    .get_mut(&CommitType::PATCH)
+                    .unwrap()
+                    .push(mock::commit::create(v.0)),
+                CommitType::OTHER => expected_commits
+                    .get_mut(&CommitType::OTHER)
+                    .unwrap()
+                    .push(mock::commit::create(v.0)),
+            };
+        });
+        (commits, expected_commits)
+    }
+
+    fn process_change_list(
+        comparison_map: &mut HashMap<CommitType, Vec<Commit>>,
+        change_list: &ChangeList,
+        version: &Version,
+        commit_type: CommitType,
+    ) {
+        for change in change_list.changes().iter() {
+            comparison_map.get_mut(&commit_type).unwrap().extend(
+                version
+                    .changes()
+                    .bucket(change.kind())
+                    .unwrap_or(&CommitBucket::new(change.kind(), change.priority().clone()))
+                    .commits()
+                    .clone(),
+            );
+        }
+    }
+
+    fn extract_actual_changes(
+        version: &Version,
+        major_changes: &ChangeList,
+        minor_changes: &ChangeList,
+        patch_changes: &ChangeList,
+        other_changes: &ChangeList,
+    ) -> HashMap<CommitType, Vec<Commit>> {
+        let mut actual_changes = mock_comparison_map();
+        process_change_list(
+            &mut actual_changes,
+            major_changes,
+            version,
+            CommitType::MAJOR,
+        );
+        process_change_list(
+            &mut actual_changes,
+            minor_changes,
+            version,
+            CommitType::MINOR,
+        );
+        process_change_list(
+            &mut actual_changes,
+            patch_changes,
+            version,
+            CommitType::PATCH,
+        );
+        process_change_list(
+            &mut actual_changes,
+            other_changes,
+            version,
+            CommitType::OTHER,
+        );
+        actual_changes
+    }
+
+    fn test_analyze_commits(
+        expected_commits: Vec<(&str, CommitType)>,
+        mock_version_num: (u32, u32, u32),
+        expected_version: &str,
+    ) {
+        let (commits, expected_changes) = mock_commits(expected_commits);
+
         let major_changes = mock::changelist::major();
         let minor_changes = mock::changelist::minor();
         let patch_changes = mock::changelist::patch();
         let other_changes = mock::changelist::other();
-        let commits = vec![
-            mock::commit::create("feat: some commit one"),
-            mock::commit::create("feat(ai): some commit two"),
-            mock::commit::create("fix(hello): some commit one"),
-            mock::commit::create(
-                "feat: some commit one\n\nBREAKING CHANGE: this will break the current version.",
-            ),
-            mock::commit::create("chore: some maintenance stuff"),
-            mock::commit::create("docs(readme): updated the readme"),
-        ];
-        let (current_major, current_minor, current_patch) = (1u32, 7u32, 4u32);
 
         let new_version = analyze_commits(
             &commits,
@@ -72,44 +178,81 @@ mod test {
             &minor_changes,
             &patch_changes,
             &other_changes,
-            (current_major, current_minor, current_patch),
+            mock_version_num,
         )
         .unwrap();
 
-        let actual_major_changes = new_version
-            .changes()
-            .bucket("BREAKING CHANGES")
-            .unwrap()
-            .commits();
-        let actual_minor_changes = new_version.changes().bucket("Feature").unwrap().commits();
-        let actual_patch_changes = new_version.changes().bucket("Fix").unwrap().commits();
-        let actual_chore_changes = new_version
-            .changes()
-            .bucket("Maintenance Items")
-            .unwrap()
-            .commits();
-        let actual_docs_changes = new_version
-            .changes()
-            .bucket("Documentation")
-            .unwrap()
-            .commits();
+        let actual_changes = extract_actual_changes(
+            &new_version,
+            &major_changes,
+            &minor_changes,
+            &patch_changes,
+            &other_changes,
+        );
 
-        let expected_major_changes = vec![mock::commit::create(
-            "feat: some commit one\n\nBREAKING CHANGE: this will break the current version.",
-        )];
-        let expected_minor_changes = vec![
-            mock::commit::create("feat: some commit one"),
-            mock::commit::create("feat(ai): some commit two"),
-        ];
-        let expected_patch_changes = vec![mock::commit::create("fix(hello): some commit one")];
-        let expected_chore_changes = vec![mock::commit::create("chore: some maintenance stuff")];
-        let expected_docs_changes = vec![mock::commit::create("docs(readme): updated the readme")];
+        assert_eq!(new_version.get(), expected_version);
+        assert_eq!(actual_changes, expected_changes);
+    }
 
-        assert_eq!(new_version.get(), "v2.0.0");
-        assert_eq!(actual_major_changes, &expected_major_changes);
-        assert_eq!(actual_minor_changes, &expected_minor_changes);
-        assert_eq!(actual_patch_changes, &expected_patch_changes);
-        assert_eq!(actual_chore_changes, &expected_chore_changes);
-        assert_eq!(actual_docs_changes, &expected_docs_changes);
+    #[test]
+    fn test_analyze_commits_random_order() {
+        test_analyze_commits(
+            vec![
+                ("feat: some commit one", CommitType::MINOR),
+                ("chore: some maintenance stuff", CommitType::OTHER),
+                ("fix(hello): some commit one", CommitType::PATCH),
+                (
+                    "feat: some commit one\n\nBREAKING CHANGE: this will break the current version.",
+                    CommitType::MAJOR,
+                ),
+                ("feat(ai): some commit two", CommitType::MINOR),
+                ("docs(readme): updated the readme", CommitType::OTHER),
+            ],
+            (1, 7, 4),
+            "v2.0.0",
+        );
+    }
+
+    #[test]
+    fn test_analyze_commits_no_history() {
+        test_analyze_commits(vec![], (1, 7, 4), "v1.7.4");
+    }
+
+    #[test]
+    fn test_analyze_commits_descending_order() {
+        test_analyze_commits(
+            vec![
+                (
+                    "feat: some commit one\n\nBREAKING CHANGE: this will break the current version.",
+                    CommitType::MAJOR,
+                ),
+                ("feat: some commit one", CommitType::MINOR),
+                ("feat(ai): some commit two", CommitType::MINOR),
+                ("fix(hello): some commit one", CommitType::PATCH),
+                ("chore: some maintenance stuff", CommitType::OTHER),
+                ("docs(readme): updated the readme", CommitType::OTHER),
+            ],
+            (4, 0, 2),
+            "v5.0.0",
+        );
+    }
+
+    #[test]
+    fn test_analyze_commits_ascending_order() {
+        test_analyze_commits(
+            vec![
+                ("feat: some commit one", CommitType::MINOR),
+                ("feat(ai): some commit two", CommitType::MINOR),
+                ("fix(hello): some commit one", CommitType::PATCH),
+                (
+                    "feat: some commit one\n\nBREAKING CHANGE: this will break the current version.",
+                    CommitType::MAJOR,
+                ),
+                ("chore: some maintenance stuff", CommitType::OTHER),
+                ("docs(readme): updated the readme", CommitType::OTHER),
+            ],
+            (1, 7, 4),
+            "v2.0.0",
+        );
     }
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,49 +1,142 @@
 mod test_env;
 
+use assert_fs::{assert::PathAssert, fixture::PathChild};
 use test_env::TestEnv;
 
 use serde_json::json;
 
+#[cfg(test)]
+mod patch_release {
+    use super::*;
+
+    #[test]
+    fn test_upgrade() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("fix: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v0.0.1\n");
+    }
+}
+
+#[cfg(test)]
+mod minor_release {
+    use super::*;
+
+    #[test]
+    fn test_upgrade() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("feat: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v0.1.0\n");
+    }
+
+    #[test]
+    fn test_upgrade_override_patch() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("fix: test");
+        env.commit("feat: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v0.1.0\n");
+    }
+
+    #[test]
+    fn test_upgrade_override_patch_offset() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("feat: test");
+        env.commit("fix: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v0.1.0\n");
+    }
+}
+
+#[cfg(test)]
+mod major_release {
+    use super::*;
+
+    #[test]
+    fn test_upgrade() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("fix: test\n\nBREAKING CHANGE: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v1.0.0\n");
+    }
+
+    #[test]
+    fn test_upgrade_override_patch() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("fix: test");
+        env.commit("fix: test\n\nBREAKING CHANGE: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v1.0.0\n");
+    }
+
+    #[test]
+    fn test_upgrade_override_minor() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("feat: test");
+        env.commit("fix: test\n\nBREAKING CHANGE: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v1.0.0\n");
+    }
+
+    #[test]
+    fn test_upgrade_override_minor_and_patch() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("fix: test");
+        env.commit("feat: test");
+        env.commit("fix: test\n\nBREAKING CHANGE: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v1.0.0\n");
+    }
+
+    #[test]
+    fn test_upgrade_override_minor_offset() {
+        let mut env = TestEnv::new(Some(json!({
+            "push_changes": false,
+        })));
+        env.commit("fix: test\n\nBREAKING CHANGE: test");
+        env.commit("feat: test");
+        env.run();
+        let tag = env.latest_tag();
+        assert_eq!(&tag, "v1.0.0\n");
+    }
+}
+
 #[test]
-fn test_release_patch_upgrade() {
+fn test_release_no_changelog() {
     let mut env = TestEnv::new(Some(json!({
+        "generate_changelog": false,
         "push_changes": false,
     })));
     env.commit("fix: test");
     env.run();
     let tag = env.latest_tag();
+    env.temp()
+        .child("CHANGELOG.md")
+        .assert(predicates::path::missing());
     assert_eq!(&tag, "v0.0.1\n");
 }
-
-#[test]
-fn test_release_minor_upgrade() {
-    let mut env = TestEnv::new(Some(json!({
-        "push_changes": false,
-    })));
-    env.commit("feat: test");
-    env.run();
-    let tag = env.latest_tag();
-    assert_eq!(&tag, "v0.1.0\n");
-}
-
-#[test]
-fn test_release_major_upgrade() {
-    let mut env = TestEnv::new(Some(json!({
-        "push_changes": false,
-    })));
-    env.commit("fix: test\n\nBREAKING CHANGE: test");
-    env.run();
-    let tag = env.latest_tag();
-    assert_eq!(&tag, "v1.0.0\n");
-}
-
-// #[test]
-// fn test_release_no_changelog() {
-//     let mut env = TestEnv::new(Some(json!({
-//         "push_changes": false,
-//     })));
-//     env.commit("fix: test");
-//     env.run();
-//     let tag = env.latest_tag();
-//     assert_eq!(&tag, "v0.0.1\n");
-// }
